39bace69452c7777ac6ef999a7df0687f078fbe7
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2024-05-02 10:58:45 +0600

    Full Module Complete With Full Practice

--- practice.js
+++ practice.js
@@ -54,3 +54,83 @@
 // }
 // var total = factorial(10);
 // console.log(total)
+
+// var age = 11;
+// // let age = 12;
+// const countryname = "Bangladesh";
+// console.log(countryname)
+// function inchToFeet(inch){
+//        console.log("Inch:", inch);
+//        var Feet = inch / 12;
+//        return Feet;
+// }
+// var TotalFeet = inchToFeet(24);
+// console.log("Feet:", TotalFeet);
+// function isEven(number){
+//        console.log(number);
+//        var reminder = number % 2;
+//        if(reminder === 0){
+//               return true;
+//        }
+//        else{
+//               return false;
+//        }
+// }
+// var waseven = isEven(24);
+// console.log(waseven);
+
+// var Number = [24, 25, 23, 45, 11, 27];
+// function findOddSum(numbers){
+//        var sum = 0;
+//        for(var i = 0; i < numbers.length; i++){
+//               var element = numbers[i];
+//               console.log(element);
+//               sum = element + sum;
+//        }
+//        return sum;
+// }
+// var total = findOddSum(Number);
+// console.log(total);
+// function getOddNumberOfAnArray(numbers){
+//        const oddNumber = [];
+//        for(let i = 0; i < numbers.length; i++){
+//               var element = numbers[i];
+//               if(element % 2 !==0){
+//                      oddNumber.push(element)
+//               }
+//        }
+//        return oddNumber;
+// }
+// const myNumber = [45, 55, 25, 30, 54, 72];
+// const number = getOddNumberOfAnArray(myNumber);
+// console.log(number);
+// function someOfNumber(numbers){
+//        let sum = 0;
+//        for(let i = 1; i <= numbers; i++){
+//               sum = sum + i;
+//               console.log(i, sum)
+//        }
+//        return sum;
+// }
+// let totalsum = someOfNumber(7);
+// console.log(totalsum)
+// function factorial(number){
+//        var sum = 1;
+//        for(let i = 1; i <= number; i++){
+//               sum = i * sum;
+//        }
+//        return sum;
+// }
+// var number = factorial(4);
+// console.log(number);
+// function whilenumber(Nmber){
+//        var sum = 1;
+//        var i = 1;
+//        while(i <= Nmber){
+//               sum = sum * i;
+//               i++
+//        }
+//        return sum;
+// }
+// var result = whilenumber(7);
+// console.log(result)
